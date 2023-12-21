2901146105472254725eb2f5c4a0ee13cbc523cb
Navpreet Virk <nvirkcom@gmail.com>
2023-12-21 10:28:12 -0800

    home: create home page, fix minor issues

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,22 +1,25 @@
 import "./Home.scss";
-import logo from "../../assets/logo.svg";
+import arcteryxLogo from "../../assets/logos/arcteryx.svg";
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <section className="home px-2 text-light">
-      <div className="container home__container pb-3 pt-5">
+      <div className="container home__container pb-3 pt-4">
         <p className="home__text text-uppercase">
           Connecting with the mountains is our reason for being, and our
-          inspiration.
+          inspiration
         </p>
-        <h1 className="display-2 home__heading">
+        <h1 className="display-1 home__heading">
           Find the perfect apparel collection for your next adventure!
         </h1>
-        <Link className="btn btn-lg btn-light home__button text-uppercase w-100">
+        <Link
+          className="btn btn-lg btn-light home__button text-uppercase w-100"
+          to="/location"
+        >
           Let's Go
         </Link>
-        <img alt="Arc'teryx logo" className="home__logo" src={logo} />
+        <img alt="Arc'teryx logo" className="home__logo" src={arcteryxLogo} />
       </div>
     </section>
   );
