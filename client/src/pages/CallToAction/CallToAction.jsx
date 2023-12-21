eb8af5cc97778a0af1439f6a01422f1e86a247d6
Navpreet Virk <nvirkcom@gmail.com>
2023-12-21 14:23:06 -0800

    calltoaction: create call to action page

--- client/src/pages/CallToAction/CallToAction.jsx
+++ client/src/pages/CallToAction/CallToAction.jsx
@@ -1,78 +1,66 @@
 import "./CallToAction.scss";
 import { Link } from "react-router-dom";
-import gallery from "../../data/images.json";
-import arcteryxLogo from "../../assets/logos/arcteryx.svg";
+import arcteryxDarkLogo from "../../assets/logos/arcteryx-dark.svg";
 import menData from "../../data/men.json";
 import womenData from "../../data/women.json";
+import { useEffect, useState } from "react";
 
 function CallToAction({ info }) {
-  const gender = "men";
-  const city_name = "Vancouver";
-  const galleryData = require(`../../data/${gender.toLowerCase()}.json`);
-  const imagesContext = require.context('../../assets', true);
-  return (
-      <div className="album py-5 bg-light gallery">
-        <div>
-          <h1 className="display-6 text-uppercase">
-            Here's a bespoke range of options for your next adventure to{" "}
-            {city_name} <br /> ...just for you
-          </h1>
-        </div>
+  const [galleryData, setGalleryData] = useState([]);
 
-    <div className="album py-5 bg-light gallery">
+  useEffect(() => {
+    if (info.gender === "Female") {
+      setGalleryData(womenData);
+    } else {
+      setGalleryData(menData);
+    }
+  }, []);
+
+  return (
+    <div className="album py-3 bg-light gallery">
       <div>
-        <h1 className="display-6 text-uppercase px-3">
-          Here's a bespoke range of options for your next adventure to {city_name} <br /> ...just for you
+        <h1 className="display-6 text-uppercase px-3 text-center">
+          Here's a bespoke range of options for your next adventure to{" "}
+          {info.city} <br />
+          <hr />
+          <p className="fw-bold text-center">Just for you!</p>
         </h1>
       </div>
-
       <div className="container">
         <div className="row">
-
-          {galleryData.map((image) => (
-            <div key={image.ID} className="col-md-4">
-              <div className="card mb-4 shadow-sm">
+          {galleryData.map((galleryItem) => (
+            <div key={galleryItem.ID} className="col-md-4">
+              <div className="card mb-4 pt-2 shadow-sm">
                 <img
-                 src={imagesContext(`../../${image.Image_name}`).default}
-                 
-                  alt={image.Name}
+                  src={`/images/${galleryItem.Image_name}`}
+                  alt={galleryItem.Name}
                   className="bd-placeholder-img card-img-top"
                   width="100%"
-                  height="225"
                 />
-
                 <div className="card-body">
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <Link to={image.url} className="btn btn-sm btn-outline-secondary">View</Link>
-                    width="100%"
-                    height="225"
-                  />
-                  <div className="card-body">
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <Link
-                          to={image.url}
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          View
-                        </Link>
-                      </div>
+                  <div className="justify-content-between align-items-center">
+                    <h5>{galleryItem.Name}</h5>
+                    <div className="btn-group w-100">
+                      <Link
+                        className="btn btn-sm btn-outline-secondary"
+                        target="_blank"
+                        to={galleryItem.url}
+                      >
+                        View
+                      </Link>
                     </div>
                   </div>
                 </div>
-
               </div>
             </div>
           ))}
         </div>
       </div>
-        <img
-          alt="Arc'teryx logo"
-          className="gallery__logo"
-          src={arcteryxLogo}
-        />
-      </div>
+      <img
+        alt="Arc'teryx logo"
+        className="gallery__logo"
+        src={arcteryxDarkLogo}
+      />
     </div>
   );
 }
