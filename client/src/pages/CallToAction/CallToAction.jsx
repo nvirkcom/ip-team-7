8415ea2bc62772c00953f814855c503fcf55e619
Aisha Atta <aisha.atta1@gmail.com>
2023-12-21 13:55:46 -0800

    add data

--- client/src/pages/CallToAction/CallToAction.jsx
+++ client/src/pages/CallToAction/CallToAction.jsx
@@ -2,23 +2,27 @@ import "./CallToAction.scss";
 import { Link } from 'react-router-dom';
 import gallery from "../../data/images.json";
 import arcteryxLogo from "../../assets/logos/arcteryx.svg";
+import menData from "../../data/men.json";
+import womenData from "../../data/women.json";
 
 function CallToAction() {
-
+  const gender = "men";
   const city_name = "Vancouver";
+  const galleryData = require(`../../data/${gender.toLowerCase()}.json`);
+  const imagesContext = require.context('../../assets', true);
   return (
-    
 
-      <div className="album py-5 bg-light gallery">
-        <div>
-          <h1 className="display-6 text-uppercase px-3">
-            Here's a bespoke range of options for your next adventure to {city_name} <br /> ...just for you
-          </h1>
-        </div>
 
-        <div className="container">
-          <div className="row">
-            {gallery.map((image) => (
+    <div className="album py-5 bg-light gallery">
+      <div>
+        <h1 className="display-6 text-uppercase px-3">
+          Here's a bespoke range of options for your next adventure to {city_name} <br /> ...just for you
+        </h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {/* {gallery.map((image) => (
               <div key={image.id} className="col-md-4">
                 <div className="card mb-4 shadow-sm">
                   <img
@@ -26,23 +30,36 @@ function CallToAction() {
                     alt={image.caption}
                     className="bd-placeholder-img card-img-top"
                     width="100%" height="225"
-                  />
-                  <div className="card-body">
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <Link to={image.url} className="btn btn-sm btn-outline-secondary">View</Link>
-                      </div>
+                  /> */}
+
+          {galleryData.map((image) => (
+            <div key={image.ID} className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <img
+                 src={imagesContext(`../../${image.Image_name}`).default}
+                 
+                  alt={image.Name}
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                />
+
+                <div className="card-body">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <Link to={image.url} className="btn btn-sm btn-outline-secondary">View</Link>
                     </div>
                   </div>
-
                 </div>
-              </div>
-            ))}
-          </div>
-        </div>
 
-        <img alt="Arc'teryx logo" className="gallery__logo" src={arcteryxLogo} />
-      </div >
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <img alt="Arc'teryx logo" className="gallery__logo" src={arcteryxLogo} />
+    </div >
 
 
 
