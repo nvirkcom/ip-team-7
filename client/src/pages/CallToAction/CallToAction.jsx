c62248a5870b2cb2ab3f93cab0fd5bfcaf2dce8e
Aisha Atta <aisha.atta1@gmail.com>
2023-12-21 11:39:04 -0800

    creat dummy json file and style gallery

--- client/src/pages/CallToAction/CallToAction.jsx
+++ client/src/pages/CallToAction/CallToAction.jsx
@@ -1,7 +1,38 @@
 import "./CallToAction.scss";
+import { Link } from 'react-router-dom';
+import gallery from "../../data/images.json";
 
 function CallToAction() {
-  return <div>CallToAction</div>;
+  return (
+
+    <div className="album py-5 bg-light">
+      <div className="container">
+        <div className="row">
+          {gallery.map((image) => (
+            <div key={image.id} className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <img
+                  src={image.url}
+                  alt={image.caption}
+                  className="bd-placeholder-img card-img-top"
+                  width="100%" height="225"
+                />
+                <div className="card-body">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <Link to={image.url} className="btn btn-sm btn-outline-secondary">View</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+
+  )
+
 }
 
 export default CallToAction;
