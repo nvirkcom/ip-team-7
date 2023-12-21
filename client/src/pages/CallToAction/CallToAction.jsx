c7e888d2ef1ce4fba22d540b02da457c67418dd2
Navpreet Virk <nvirkcom@gmail.com>
2023-12-21 13:51:35 -0800

    fix: fix call to action conflict

--- client/src/pages/CallToAction/CallToAction.jsx
+++ client/src/pages/CallToAction/CallToAction.jsx
@@ -6,53 +6,57 @@ import arcteryxLogo from "../../assets/logos/arcteryx.svg";
 function CallToAction({ info }) {
   const city_name = "Vancouver";
   return (
-    
-
-      <div className="album py-5 bg-light gallery">
-        <div>
-          <h1 className="display-6 text-uppercase px-3">
-            Here's a bespoke range of options for your next adventure to {city_name} <br /> ...just for you
-          </h1>
-        </div>
     <div className="album py-5 bg-light gallery">
       <div>
-        <h1 className="display-6 text-uppercase">
+        <h1 className="display-6 text-uppercase px-3">
           Here's a bespoke range of options for your next adventure to{" "}
           {city_name} <br /> ...just for you
         </h1>
       </div>
+      <div className="album py-5 bg-light gallery">
+        <div>
+          <h1 className="display-6 text-uppercase">
+            Here's a bespoke range of options for your next adventure to{" "}
+            {city_name} <br /> ...just for you
+          </h1>
+        </div>
 
-      <div className="container">
-        <div className="row">
-          {gallery.map((image) => (
-            <div key={image.id} className="col-md-4">
-              <div className="card mb-4 shadow-sm">
-                <img
-                  src={image.url}
-                  alt={image.caption}
-                  className="bd-placeholder-img card-img-top"
-                  width="100%"
-                  height="225"
-                />
-                <div className="card-body">
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <Link
-                        to={image.url}
-                        className="btn btn-sm btn-outline-secondary"
-                      >
-                        View
-                      </Link>
+        <div className="container">
+          <div className="row">
+            {gallery.map((image) => (
+              <div key={image.id} className="col-md-4">
+                <div className="card mb-4 shadow-sm">
+                  <img
+                    src={image.url}
+                    alt={image.caption}
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                  />
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <Link
+                          to={image.url}
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          View
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
 
-      <img alt="Arc'teryx logo" className="gallery__logo" src={arcteryxLogo} />
+        <img
+          alt="Arc'teryx logo"
+          className="gallery__logo"
+          src={arcteryxLogo}
+        />
+      </div>
     </div>
   );
 }
